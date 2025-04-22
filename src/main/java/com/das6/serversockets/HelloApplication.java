c24d4c7698cc8ad64dbc72c30b31987875b1df06
ivanfluo <ivanfluo@gmail.com>
2025-04-21 19:25:12 -0600

    Ivan Hernandez: Se implementa movimiento de ventana y ventana en esquina superior izquierda

--- src/main/java/com/das6/serversockets/HelloApplication.java
+++ src/main/java/com/das6/serversockets/HelloApplication.java
@@ -2,7 +2,9 @@ package com.das6.serversockets;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
@@ -13,6 +15,18 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/com/das6/serversockets/Cliente/principal-cliente.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
+
+        Rectangle2D pantalla = Screen.getPrimary().getVisualBounds();
+
+        double anchoVentana = 366;
+        double altoVentana = 251;
+
+        double posicionX = pantalla.getMaxX() - anchoVentana;
+        double posicionY = pantalla.getMinY();
+
+        stage.setX(posicionX);
+        stage.setY(posicionY);
+
         stage.initStyle(StageStyle.UNDECORATED);
         stage.setScene(scene);
         stage.show();
